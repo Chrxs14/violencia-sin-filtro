88f76ae45f5f1c6f41991a88a7ad0382340e50c0
Chrxs14 <trabajo.ochoachristopher@gmail.com>
2024-11-02 21:48:32 -0500

    feat: create project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./components/Home/Home"
+import  Demo  from "./components/Demo/Demo"
+
+function App() {
+  return (
+    <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/demo" element={<Demo />} />
+        </Routes>
+    </>
+  );
+}
+
+export { App }
