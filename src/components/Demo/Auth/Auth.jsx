a58f5c718c4634e4a78cff2007dda3ac74cac4a2
Chrxs14 <trabajo.ochoachristopher@gmail.com>
2024-11-04 11:50:39 -0500

    feat: se implementan los componentes para inicio de sesion y registro de usuario

--- src/components/Demo/Auth/Auth.jsx
+++ src/components/Demo/Auth/Auth.jsx
@@ -1,43 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal } from "../../../utils/Modal";
 import { LiaTimesSolid } from "react-icons/lia";
-import { FcGoogle } from "react-icons/fc";
-import { MdFacebook } from "react-icons/md";
-import { AiOutlineMail } from "react-icons/ai";
+import { SignIn } from "./SignIn";
+import { SignUp } from "./SignUp";
+import { SignInOpt } from "./SignInOpt";
 
 export const Auth = () => {
+  const [signReq, setSignReq] = useState("");
+  const [modal, setModal] = useState(true);
+
+  const hidden = modal ? "visible opacity-100" : "invisible opacity-0";
+
   return (
-    <Modal>
+    <Modal modal={modal} setModal={setModal} hidden={hidden}>
       <section
-        className="z-50 fixed top-0 bottom-0 left-0 md:left-[10rem] 
-        overflow-auto right-0 md:right-[10rem] bg-white shadows"
+        className={`z-50 fixed top-0 bottom-0 left-0 md:left-[10rem] 
+        overflow-auto right-0 md:right-[10rem] bg-white shadows
+        ${modal ? "visible opacity-100" : "invisible opacity-0"}
+        transition-all duration-500
+        ${hidden}`}
       >
-        <button className="absolute top-8 right-8 text-2xl hover:apacity-50">
+        <button 
+        onClick={()=>setModal(false)}
+        className="absolute top-8 right-8 text-2xl hover:opacity-50">
           <LiaTimesSolid />
         </button>
-        <div className="flex flex-col justify-center items-center fap-[3rem]">
-          <>
-            <h2 className="text-2x1 pt-[5rem]">Bienvenido</h2>
-            <div>
-                <Button icon={<FcGoogle/>} text={"Sign In With Google"}/>
-                <Button icon={<MdFacebook/>} text={"Sign In With Facebook"}/>
-                <Button icon={<AiOutlineMail/>} text={"Sign In With Mail"}/>
-            </div>
-          </>
+        <div className="flex flex-col justify-center items-center gap-[3rem]">
+          {signReq === "" ? (
+            <SignInOpt setSignReq={setSignReq} />
+          ) : signReq === "sign-in" ? (
+            <SignIn setSignReq={setSignReq} />
+          ) : signReq === "sign-up" ? (
+            <SignUp setSignReq={setSignReq} />
+          ) : null}
+          <p className="md:w-[30rem] mx-auto text-center text-sm mb-[3rem]">
+            Click "Sign In" to agree to foro terms of service and acknowledge
+            that foro's privacy policy applies to you.
+          </p>
         </div>
       </section>
     </Modal>
-  );
-};
-
-const Button = ({ icon, text, click }) => {
-  return (
-    <button
-      className="flex items-center gap-10 sm:w-[20rem] border border-black
-        px-3 py-2 rounded-full"
-    >
-      {icon}
-      {text}
-    </button>
   );
 };
