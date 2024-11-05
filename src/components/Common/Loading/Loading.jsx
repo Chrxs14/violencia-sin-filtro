fabe7d5376fc145bd2fec38c6fab1138f09d10b0
Chrxs14 <trabajo.ochoachristopher@gmail.com>
2024-11-04 23:43:09 -0500

    feat: creacion de componente Loading en Commons

---
+++ src/components/Common/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Loading = () => {
+  return (
+    <div className="fixed insed-0 grid place-items-center bg-white z-30">
+      <img src="/loading2.gif" alt="loading" />
+    </div>
+  );
+};
