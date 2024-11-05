4c897e26c29c4f04763d38fe5b1f75afe7b2f911
Chrxs14 <trabajo.ochoachristopher@gmail.com>
2024-11-04 23:42:32 -0500

    feat: implementacion de useNavigate

--- src/components/Demo/Auth/Auth.jsx
+++ src/components/Demo/Auth/Auth.jsx
@@ -4,9 +4,11 @@ import { LiaTimesSolid } from "react-icons/lia";
 import { SignIn } from "./SignIn";
 import { SignUp } from "./SignUp";
 import { SignInOpt } from "./SignInOpt";
+import { useNavigate } from "react-router-dom";
 
 export const Auth = ({ modal, setModal }) => {
   const [signReq, setSignReq] = useState("");
+  const navigate = useNavigate();
 
   const hidden = modal ? "visible opacity-100" : "invisible opacity-0";
 
@@ -14,10 +16,8 @@ export const Auth = ({ modal, setModal }) => {
     <Modal modal={modal} setModal={setModal} hidden={hidden}>
       <section
         className={`z-50 fixed top-0 bottom-0 left-0 md:left-[10rem] 
-        overflow-auto right-0 md:right-[10rem] bg-white shadows
-        ${modal ? "visible opacity-100" : "invisible opacity-0"}
-        transition-all duration-500
-        ${hidden}`}
+        overflow-auto right-0 md:right-[10rem] bg-white shadows transition-all duration-500
+        ${modal ? "visible opacity-100" : "invisible opacity-0"}`}
       >
         <button
           onClick={() => setModal(false)}
