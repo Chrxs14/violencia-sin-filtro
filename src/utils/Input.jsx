82e4be33360431bc563b9ff29c6151532c3fa0f8
Chrxs14 <trabajo.ochoachristopher@gmail.com>
2024-11-06 18:43:14 -0500

    feat: implementacion de handleChange en componente Input

--- src/utils/Input.jsx
+++ src/utils/Input.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export const Input = ({ type, title }) => {
+export const Input = ({ type, title, form, setForm }) => {
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="flex flex-col gap-2 ">
       <label className="text-sm capitalize">{title}</label>
@@ -8,6 +12,7 @@ export const Input = ({ type, title }) => {
         className="text-center border-b border-black outline-none"
         type={type}
         name={title}
+        onChange={handleChange}
       />
     </div>
   );
