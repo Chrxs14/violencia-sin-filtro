cbe4acb34530711a621b25bae4ff475b2ea43d59
Chrxs14 <trabajo.ochoachristopher@gmail.com>
2024-11-06 18:41:25 -0500

    feat: envio de modal a componente SignUp

--- src/components/Demo/Auth/Auth.jsx
+++ src/components/Demo/Auth/Auth.jsx
@@ -31,7 +31,7 @@ export const Auth = ({ modal, setModal }) => {
           ) : signReq === "sign-in" ? (
             <SignIn setSignReq={setSignReq} />
           ) : signReq === "sign-up" ? (
-            <SignUp setSignReq={setSignReq} />
+            <SignUp setModal={setModal} setSignReq={setSignReq} />
           ) : null}
           <p className="md:w-[30rem] mx-auto text-center text-sm mb-[3rem]">
             Click "Sign In" to agree to foro terms of service and acknowledge
