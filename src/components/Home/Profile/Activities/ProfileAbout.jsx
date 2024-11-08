17ffb5979600918f858609970e0fc17f994fe58d
chrxs14 <trabajo.ochoachristopher@gmail.com>
2024-11-08 17:38:44 -0500

    feat: se crea funcionalidad para componente ProfileAbout

--- src/components/Home/Profile/Activities/ProfileAbout.jsx
+++ src/components/Home/Profile/Activities/ProfileAbout.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export const ProfileAbout = ({getUserData}) => {
+export const ProfileAbout = ({ getUserData, setEditModal }) => {
   return (
-    <div>ProfileAbout</div>
-  )
-}
+    <div className="w-full">
+      <p className="text-2xl firts-letter:uppercase">
+        {getUserData?.bio || getUserData?.username + " Has no bio"}
+      </p>
+      <div className="text-right">
+        <button
+          onClick={() => setEditModal(true)}
+          className="border border-black py-2 px-5 rounded-full text-black mt-[3rem]"
+        >
+          Edit
+        </button>
+      </div>
+    </div>
+  );
+};
